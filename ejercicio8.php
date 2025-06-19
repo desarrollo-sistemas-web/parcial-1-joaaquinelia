4d91d92143c002f0bdfeadcaa60217dda2010d7c
joaaquinelia <joaquinelia294@gmail.com>
2025-06-19 20:20:21 -0300

    Corrijo parcial 1

--- ejercicio8.php
+++ ejercicio8.php
@@ -3,6 +3,6 @@
 //Ejercicio 8: Completa el siguiente código para imprimir los números del 1 al 10 utilizando un bucle for.
 //Complejidad: BAJA (1 punto)
 
-for ($i=0; $i<=10; $i++) {
-    echo $i;
+for ($i=1; $i<=10; $i++) {
+    echo $i . "<br>";
 }
